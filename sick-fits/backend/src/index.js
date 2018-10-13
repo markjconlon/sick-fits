49c151749767b0ab0d05a5ab1ecb03f7c15b3605
markjconlon <markjconlon@gmail.com>
2018-10-13 13:11:27 -0400

    created index.js to ensure the db and yoga server are connected and env is available

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,1 +1,19 @@
 // let's go!
+// entry point to app
+require('dotenv').config({path: 'variables.env'});
+const createServer = require('createServer');
+const db = require('./db');
+
+const serer = createServer();
+
+// TODO Use express middleware to handle cookies (JWT)
+// TODO Use express middleware to handle populate current user
+
+server.start({
+  cors: {
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  }
+}, deets => {
+  console.log(` Server is now running on port http:/localhost:${deets.port}`);
+});
